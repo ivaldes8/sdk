d59fcca7b3054bdcde696aadffb1acbab5204a75
Iván González Valdés <kaosolution8@gmail.com>
2024-09-30 22:54:22 +0200

    deploy to gh-pages

---
+++ docs/assets/Header.stories-CDdpBpK3.js
@@ -0,0 +1,7 @@
+import{f as o}from"./index-DcZGqPg1.js";import{H as p}from"./Header-D3WShn5D.js";import"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";import"./index-rNTiGNI1.js";import"./Button-BbxwbYmZ.js";import"./Icon-r3XCP1xN.js";const H={title:"Molecules/Header",component:p,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var n,c,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(m=(c=r.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const O=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,O as __namedExportsOrder,H as default};
---
+++ docs/assets/Icon.stories-BwkMFAkt.js
@@ -0,0 +1,6 @@
+import{I as a}from"./Icon-r3XCP1xN.js";import{f as t}from"./index-DcZGqPg1.js";import"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const p={title:"Atoms/Icon",component:a,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onClick:t()},argTypes:{onClick:{control:!1}}},o={args:{size:"medium",icon:"arrowDown"}};var r,e,s;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    size: "medium",
+    icon: "arrowDown"
+  }
+}`,...(s=(e=o.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const d=["Medium"];export{o as Medium,d as __namedExportsOrder,p as default};
